9d0065a5fe01ba8fe595851681b52dc710a59354
Chris Fagan <cfagan88@hotmail.co.uk>
2025-02-16 17:30:55 +0000

    Add profile picture

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,4 +1,5 @@
 import RevealOnScroll from "../RevealOnScroll";
+import Image from "../../assets/CF - square.png";
 
 function Home() {
   return (
@@ -8,9 +9,14 @@ function Home() {
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="h-[7rem] text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text leading-right text-transparent">
+          <h1 className="h-[6rem] text-5xl md:text-7xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text leading-right text-transparent">
             Chris Fagan
           </h1>
+          <img
+            src={Image}
+            alt="Profile Picture"
+            className="w-50 rounded-full mx-auto mb-6"
+          />
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
             Full-Stack Software Developer
           </p>
