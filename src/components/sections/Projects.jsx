383b18cb2dc8dcf1163d428d7fedbcb0da430a0a
Chris Fagan <cfagan88@hotmail.co.uk>
2025-03-05 16:25:32 +0000

    Add calculator project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,7 +13,7 @@ function Projects() {
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="p-8 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all relative">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all relative">
               <h3 className="text-xl font-semibold mb-2">
                 Northcoders News Backend
               </h3>
@@ -127,6 +127,36 @@ function Projects() {
                 <div className="absolute right-3 bottom-3">
                   <a
                     href="https://github.com/cfagan88/React-Movie-DB"
+                    className="text-gray-300 hover:text-blue-300 hover:underline transition-colors"
+                  >
+                    View project on GitHub
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all relative">
+              <h3 className="text-xl font-semibold mb-2">Calculator</h3>
+              <p className="text-gray-400 mb-4 font-extralight">
+                A basic calculator app with core addition, subtraction, division
+                and multiplication functionality. Focus was placed on ensuring
+                only valid calculations could be entered!
+              </p>
+              <div className="flex flex-wrap gap-2 mb-6">
+                {["JavaScript", "React", , "Vite", "Tailwind"].map(
+                  (skill, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                    >
+                      {skill}
+                    </span>
+                  )
+                )}
+
+                <div className="absolute right-3 bottom-3">
+                  <a
+                    href="https://github.com/cfagan88/calculator"
                     className="text-gray-300 hover:text-blue-300 hover:underline transition-colors"
                   >
                     View project on GitHub
